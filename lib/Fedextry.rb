3a4ab89368d1c7c9d38f42cf04b3d9dcb4e230e5
erickhansx <erickhansx@gmail.com>
2023-12-22 13:08:10 -0600

    Add Rates class to Fedextry module

--- lib/Fedextry.rb
+++ lib/Fedextry.rb
@@ -1,8 +1,56 @@
 # frozen_string_literal: true
 
+require "httparty"
+require "nokogiri"
+require "dotenv"
+require "erb"
+require "json"
+
 require_relative "Fedextry/version"
 
 module Fedextry
-  class Error < StandardError; end
-  # Your code goes here...
+  class Rates # rubocop:disable Style/Documentation
+    def self.get(credentials, quote_params)
+      url = "https://wsbeta.fedex.com:443/xml"
+      body = build_request_body(credentials, quote_params)
+
+      response = HTTParty.post(url, body: body)
+      parse_response(response)
+    end
+
+    def self.build_request_body(credentials, quote_params)
+      template_path = File.expand_path("templates/fedex_rate_request.xml.erb", __dir__)
+      template = File.read(template_path)
+      ERB.new(template).result(binding)
+    end
+
+    def self.parse_response(response) # rubocop:disable Metrics/MethodLength,Metrics/AbcSize
+      doc = Nokogiri::XML(response.body)
+
+      rates = doc.xpath("//*[local-name()='RateReplyDetails']").map do |detail|
+        service_type = detail.at_xpath("*[local-name()='ServiceType']").text
+
+        rate_detail = detail.at_xpath("*[local-name()='RatedShipmentDetails']/*[local-name()='ShipmentRateDetail']")
+
+        price = rate_detail.at_xpath("*[local-name()='TotalNetCharge']/*[local-name()='Amount']").text.to_f
+        currency = rate_detail.at_xpath("*[local-name()='TotalNetCharge']/*[local-name()='Currency']").text
+
+        service_token = service_type.gsub(/\s+/, "_").upcase
+
+        {
+          price: price,
+          currency: currency,
+          service_level: {
+            name: service_type,
+            token: service_token
+          }
+        }
+      end
+
+      # Filter out any entries with missing data
+      rates.select { |rate| (rate[:price]).positive? && !rate[:currency].empty? && !rate[:service_level][:name].empty? }
+    rescue Nokogiri::XML::SyntaxError => e
+      { error: "Failed to parse XML: #{e.message}" }
+    end
+  end
 end
